49ebebdcbd8e5ee755fa4b3913ec2794ef1fe7fe
kapkappa <roman.kupry@gmail.com>
2021-09-29 10:00:55 +0300

    task solved

--- task_05/vec_transpose.cu
+++ task_05/vec_transpose.cu
@@ -4,11 +4,9 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-const int SIZE = 32;
+const int SIZE = 1024;
 
 __global__ void transpose(int *V, int n) {
-//    __shared__ int array[SIZE];
-
     int Idx = blockDim.x * blockIdx.x + threadIdx.x;
     if (Idx <=  n/2) {
         int tmp = V[Idx];
@@ -17,34 +15,122 @@ __global__ void transpose(int *V, int n) {
     }
 }
 
+__global__ void static_trans(int *Vec, int N) {
+    __shared__ int array[SIZE];
+
+    int Idx = threadIdx.x; //we have only one block - condition
+    array[Idx] = Vec[Idx];
+
+    __syncthreads();
+
+    Vec[Idx] = array[N-Idx-1];
+}
+
+__global__ void dynamic_trans(int *Vec, int N) {
+    extern __shared__ int array[];
+//    int *array_ptr = array;
+
+    int Idx = threadIdx.x;
+    array[Idx] = Vec[Idx];
+
+    __syncthreads();
+
+    Vec[Idx] = array[N-Idx-1];
+}
+
+
 int main(int argc, char **argv) {
     assert(argc==2);
     int n = atoi(argv[1]);
 
     size_t size = n * sizeof(int);
     int *V = (int*)malloc(size);
+    int *V_t = (int*)malloc(size);
 
     for (int i = 0; i < n; i++) {
         V[i] = i;
     }
 
     int block = 1024;
-    int grid = (n / 2 - 1) / block + 1;
+    int grid = 1;
+//    int grid = (n / 2 - 1) / block + 1;
 
-    int *V_t;
-    cudaMalloc(&V_t, size);
-    cudaMemcpy(V_t, V, size, cudaMemcpyHostToDevice);
+    int *V_dev;
+    cudaMalloc(&V_dev, size);
+    cudaMemcpy(V_dev, V, size, cudaMemcpyHostToDevice);
 
-    transpose<<<grid, block>>>(V_t, n);
+cudaEvent_t start, stop;
+cudaEventCreate(&start);
+cudaEventCreate(&stop);
 
-    cudaDeviceSynchronize();
+cudaEventRecord(start);
 
-    cudaMemcpy(V, V_t, size, cudaMemcpyDeviceToHost);
+    transpose<<<grid, block>>>(V_dev, n);
+
+cudaDeviceSynchronize();
+cudaEventRecord(stop);
+
+    cudaMemcpy(V_t, V_dev, size, cudaMemcpyDeviceToHost);
+
+cudaEventSynchronize(stop);
+
+float ms = 0;
+cudaEventElapsedTime(&ms, start, stop);
+std::cout << "Common time is: " << ms << std::endl;
 
     for (int i = 0; i < n; i++) {
-        std::cout << V[i] << " ";
+        std::cout << V_t[i] << " ";
     }
     std::cout << std::endl;
+
+/////////////////////////////////////////////////
+//STATIC SHARED
+
+    cudaMemcpy(V_dev, V, size, cudaMemcpyHostToDevice);
+
+cudaEventRecord(start);
+
+    static_trans<<<grid, block, n>>>(V_dev, n);
+
+cudaDeviceSynchronize();
+cudaEventRecord(stop);
+
+    cudaMemcpy(V_t, V_dev, size, cudaMemcpyDeviceToHost);
+
+cudaEventSynchronize(stop);
+
+ms = 0;
+cudaEventElapsedTime(&ms, start, stop);
+std::cout << "Static sh m time is: " << ms << std::endl;
+   for (int i = 0; i < n; i++) {
+        std::cout << V_t[i] << " ";
+    }
+    std::cout << std::endl;
+
+/////////////////////////////////////////////////
+//DYNAMIC SHARED
+
+cudaMemcpy(V_dev, V, size, cudaMemcpyHostToDevice);
+
+cudaEventRecord(start);
+
+    dynamic_trans<<<grid, block, n>>>(V_dev, n);
+
+cudaDeviceSynchronize();
+cudaEventRecord(stop);
+
+    cudaMemcpy(V_t, V_dev, size, cudaMemcpyDeviceToHost);
+
+cudaEventSynchronize(stop);
+
+ms = 0;
+cudaEventElapsedTime(&ms, start, stop);
+std::cout << "Dynamic sh m time is: " << ms << std::endl;
+   for (int i = 0; i < n; i++) {
+        std::cout << V_t[i] << " ";
+    }
+    std::cout << std::endl;
+
 
     free(V);
     cudaFree(V_t);
