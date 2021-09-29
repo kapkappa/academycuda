0c2a14c2228c10bcc0939c46f7ada82186f75ff9
kapkappa <roman.kupry@gmail.com>
2021-09-29 17:40:58 +0300

    new tests

--- task_08/test.sh
+++ task_08/test.sh
@@ -3,6 +3,7 @@
 #for size in 100 512 1024; do
 for version in 0 1; do
 for size in 1048576 268435456; do
+#for size in 512 2048; do
 
 mpisubmit.pl --stdout log.out.$version.$size --stderr gpu.err.$version.$size --gpu prog_gpu -- $size $version
 
