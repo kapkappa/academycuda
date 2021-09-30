ca4230774f69db7b41e08c9bc583bf7291843f33
kapkappa <roman.kupry@gmail.com>
2021-09-30 13:58:58 +0300

    nvprof trace addded

--- task_06/test.sh
+++ task_06/test.sh
@@ -4,10 +4,8 @@ for version in old new; do
 #for size in 1048576 268435456; do
 #for size in 512 2048; do
 
-mpisubmit.pl --stdout log.out.$version --stderr gpu.err.$version --gpu prog_$version
+mpisubmit.pl --stdout gpu.out.$version --stderr gpu.err.$version --gpu prog_$version
+mpisubmit.pl --stdout gpu.nprof.out.$version --stderr gpu.nprof.err.$version --gpu ~/local_cuda/bin/nvprof -- --analysis-metrics -f -o short.$version ./prog_$version
 
 #done
 done
-
-#rm err.log out.log
-#mpisubmit.pl --stdout out.log --stderr err.log --gpu ~/local_cuda/bin/nvprof -- --analysis-metrics -o short ~/academycuda/task_08/prog_gpu 2000000 1
