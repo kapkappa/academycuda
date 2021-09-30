7f675cae1af5d2f3022cbcd059d348ea9fb815d0
kapkappa <roman.kupry@gmail.com>
2021-09-30 13:48:19 +0300

    nvprofile trace changed

--- task_08/test.sh
+++ task_08/test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-#for size in 100 512 1024; do
-for version in 0 1; do
-for size in 1048576 268435456; do
+#for version in 0 1; do
+#for size in 1048576 268435456; do
 #for size in 512 2048; do
 
-mpisubmit.pl --stdout log.out.$version.$size --stderr gpu.err.$version.$size --gpu prog_gpu -- $size $version
+#pisubmit.pl --stdout log.out.$version.$size --stderr gpu.err.$version.$size --gpu prog_gpu -- $size $version
 
-done
-done
+#done
+#done
+
+rm gpu.err gpu.out
+mpisubmit.pl --stdout gpu.out --stderr gpu.err --gpu ~/local_cuda/bin/nvprof -- --analysis-metrics -f -o short ~/academycuda/task_08/prog_gpu 268430000 1
